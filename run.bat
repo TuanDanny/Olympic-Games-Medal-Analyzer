8ee2e6d6c0e40c9f885a7bdb0bc49d9385561b3e
TuanDanny <88261316+TuanDanny@users.noreply.github.com>
2025-05-27 23:17:29 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,1 @@
+java --module-path "javafx-sdk-24.0.1\lib" --add-modules javafx.controls,javafx.fxml,javafx.swing --add-exports=javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED -jar Olympic_Games_Medal_Analyzer.jar
